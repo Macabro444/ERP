30bd96480e19ae976da29f53138eee442dbdac25
JorgeTrejoCuevas <jorgeatrejo2@gmail.com>
2026-03-10 00:11:09 -0600

    Actualizacion de Pagina de Inicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
